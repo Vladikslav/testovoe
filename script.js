e5a7af95441d394a8e2a21cc4aa821b8f2c45065
Aleksandr Grigoriy <Kurakin_V@baza.ngo.ru>
2024-01-17 16:28:35 +0400

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    document.querySelector('.page__bg').classList.add('page__bg--active');
+    document.querySelector('.point__description').classList.add('point__description--active');
+    document.querySelector('.point__title').classList.add('point__title--active');
+    document.querySelector('.line').classList.add('line--active');
+    document.querySelector('.point__circle--second').classList.add('point__circle--second--active');
+}
